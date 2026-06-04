f8ad11c0a352ba36e46407b27de4846b84483d84
Adam <adamyork@users.noreply.github.com>
2026-06-04 13:31:31 -0400

    feature: getting the debug runtime configured

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,9 @@
 @file:OptIn(org.jetbrains.kotlin.gradle.ExperimentalWasmDsl::class)
 
 plugins {
-    id("org.jetbrains.kotlin.multiplatform") version "2.3.0"
-    id("org.jetbrains.kotlin.plugin.serialization") version "2.3.0"
-    id("com.google.devtools.ksp") version "2.3.0"
+    id("org.jetbrains.kotlin.multiplatform") version "2.3.20"
+    id("org.jetbrains.kotlin.plugin.serialization") version "2.3.20"
+    id("com.google.devtools.ksp") version "2.3.9"
 }
 
 group = "com.github.adamyork"
@@ -20,9 +20,6 @@ kotlin {
             implementation("me.tatarka.inject:kotlin-inject-runtime:0.7.2")
             implementation("io.github.reactivecircus.cache4k:cache4k:0.14.0")
         }
-        commonTest.dependencies {
-            implementation("org.jetbrains.kotlin:kotlin-test:2.3.0")
-        }
         wasmJsMain.dependencies {
             implementation("org.jetbrains.kotlinx:kotlinx-browser:0.3")
             implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.11.0")
@@ -33,24 +30,4 @@ kotlin {
 
 dependencies {
     add("kspWasmJs", "me.tatarka.inject:kotlin-inject-compiler-ksp:0.7.2")
-}
-
-// 1. Register your custom copy task
-val assembleOutput = tasks.register<Copy>("assembleOutput") {
-    group = "build"
-    description = "Copies distribution and static resource files to the build/output directory."
-
-    from(layout.projectDirectory.dir("build/dist/wasmJs/productionExecutable"))
-    from(layout.projectDirectory.dir("src/main/resources/static"))
-    into(layout.buildDirectory.dir("output"))
-
-    duplicatesStrategy = DuplicatesStrategy.INCLUDE
-
-    // Make this task wait until the WebAssembly distribution is generated
-    dependsOn("wasmJsBrowserDistribution")
-}
-
-// 2. Hook your task into the standard build lifecycle
-tasks.named("build") {
-    dependsOn(assembleOutput)
 }
